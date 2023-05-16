47c3cfeb7f79d4fc34af4c07fae7882c49f0c229
rkrisztian <2960445+rkrisztian@users.noreply.github.com>
2023-05-16 13:30:51 +0200

    Change the endpoints so they are easier to reverse proxy
    Old:
    
        GUI: http://localhost:1717/secrets
        REST: http://localhost:1717/api/secrets
    
    New:
    
        GUI: http://localhost:1717/secrets
        REST: http://localhost:1717/secrets/api
    
    This way you can have an NGINX configuration that proxies "/secrets" to "/secrets". With the old config, either you needed two
    location definitions, or an extra prefix in the path requested by the client.

--- webservice/src/test/java/com/vary/tenantsecrets/tenantsecrets/TenantsecretsApplicationTests.java
+++ webservice/src/test/java/com/vary/tenantsecrets/tenantsecrets/TenantsecretsApplicationTests.java
@@ -1,36 +1,45 @@
 package com.vary.tenantsecrets.tenantsecrets;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(
-		initializers = PropertyOverrideContextInitializer.class,
-		classes = TenantsecretsApplication.class)
+        initializers = PropertyOverrideContextInitializer.class,
+        classes = TenantsecretsApplication.class)
 class TenantsecretsApplicationTests {
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@Test
-	void shouldEncrypt() {
-		ResponseEntity<String> response = restTemplate.postForEntity(
-				"http://localhost:" + port + "/api/secrets/my_group", "my secret", String.class);
+    @Test
+    void shouldEncrypt() {
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                "http://localhost:" + port + "/secrets/api/my_group", "my secret", String.class);
 
-		assertEquals(201, response.getStatusCode().value());
-		assertTrue(response.getBody().startsWith("AES:"));
-	}
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).startsWith("AES:");
+    }
+
+    @Test
+    void shouldLoadUi() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/secrets", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("<form");
+    }
 
 }
